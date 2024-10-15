c3b56dad36f0d7aa4b26c926b90d315847dddb5f
Luís Fernando Nascimento <105133507+LuisFNascimento@users.noreply.github.com>
2024-10-15 17:12:14 -0300

    Adicionando atualização do código

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -23,4 +23,135 @@ public class Serie {
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
     }
+
+    
+
+    /**
+     * @return String return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return String return the Ano
+     */
+    public String getAno() {
+        return Ano;
+    }
+
+    /**
+     * @param Ano the Ano to set
+     */
+    public void setAno(String Ano) {
+        this.Ano = Ano;
+    }
+
+    /**
+     * @return Integer return the totalTemporadas
+     */
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    /**
+     * @param totalTemporadas the totalTemporadas to set
+     */
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    /**
+     * @return Double return the avalicao
+     */
+    public Double getAvalicao() {
+        return avalicao;
+    }
+
+    /**
+     * @param avalicao the avalicao to set
+     */
+    public void setAvalicao(Double avalicao) {
+        this.avalicao = avalicao;
+    }
+
+    /**
+     * @return Categoria return the genero
+     */
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return String return the atores
+     */
+    public String getAtores() {
+        return atores;
+    }
+
+    /**
+     * @param atores the atores to set
+     */
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    /**
+     * @return String return the poster
+     */
+    public String getPoster() {
+        return poster;
+    }
+
+    /**
+     * @param poster the poster to set
+     */
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    /**
+     * @return String return the sinopse
+     */
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    /**
+     * @param sinopse the sinopse to set
+     */
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return   "genero= [" + genero + 
+                 ", titulo=" + titulo +
+                 ", Ano=" + Ano + 
+                 ", totalTemporadas=" + totalTemporadas + 
+                 ", avalicao=" + avalicao + 
+                 ", atores=" + atores + 
+                 ", poster=" + poster + 
+                 ", sinopse=" + sinopse + "]";
+    }
+
+    
+
 }
